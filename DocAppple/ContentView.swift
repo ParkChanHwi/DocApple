bdccddbe05060e935d7b2e94df7fdac71acd6056
Parkchanhwi <cksgnl0523@gmail.com>
2025-01-26 02:43:27 +0900

    Initial Commit

--- DocAppple/ContentView.swift
+++ DocAppple/ContentView.swift
@@ -1,26 +1,51 @@
-// image source by  icons8 : https://icons8.kr/icon/MNKxQwOqbO8D/apple-fruit, flaticon : https://www.flaticon.com/free-icon/open-book_5773837
-//  ContentView.swift
-//  DocAppple
-//
-//  Created by 박찬휘 on 1/22/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var query: String = ""
+    @State private var books: [Book] = []
+    @State private var errorMessage: String?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Search for books", text: $query)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button("Search") {
+                    searchBooks()
+                }
+                .padding()
+
+                if let errorMessage = errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                        .padding()
+                }
+
+                List(books, id: \.isbn) { book in
+                    VStack(alignment: .leading) {
+                        Text(book.title)
+                            .font(.headline)
+                        Text(book.author)
+                            .font(.subheadline)
+                        Text(book.publisher)
+                            .font(.footnote)
+                    }
+                }
+            }
+            .navigationTitle("Book Search")
         }
-        .padding()
+    }
+
+    func searchBooks() {
+        APIManager.shared.fetchBooks(query: query) { result in
+            if let result = result {
+                books = result
+                errorMessage = nil
+            } else {
+                errorMessage = "Failed to fetch books from API."
+            }
+        }
     }
 }
-
-#Preview {
-    ContentView()
-}
-
-
